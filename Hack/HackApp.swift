beed6f8f7cc721e423bd04deac2325641cb4a3c5
gael102 <gael.pato04@gmail.com>
2025-06-07 14:22:41 -0600

    Primer commit del proyecto HackApp

---
+++ Hack/HackApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackApp.swift
+//  Hack
+//
+//  Created by Gael Gaytan on 07/06/25.
+//
+
+import SwiftUI
+
+@main
+struct HackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
